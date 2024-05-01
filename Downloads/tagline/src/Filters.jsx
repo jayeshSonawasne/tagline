19c3020e0a4a53920f401137f32fec6e9edb8ccd
Jayesh <jayesh.sonawane@lemonlabs.in>
2024-05-01 11:44:33 +0530

    refactor code

--- Downloads/tagline/src/Filters.jsx
+++ Downloads/tagline/src/Filters.jsx
@@ -1,76 +1,131 @@
 import React, { useState } from "react";
 import { Data } from "./data";
-
 const appliedFilters = [];
 
 export default function Filters() {
   const [data, setData] = useState(Data);
-  const headers = [...new Set(Data.flatMap(obj => Object.keys(obj)))];
-  const allKeys = new Set(Object.keys(Data[0]).filter(key => key !== 'id'));
-  const [filterList, setFilterList] = useState(Array.from(allKeys));
+  const headers = Array.from(new Set(Data.flatMap(obj => Object.keys(obj))));
+  const allKeys = new Set();
+  Data.forEach(obj => Object.keys(obj).forEach(key => allKeys.add(key)));
+  allKeys.delete('id');
+  const allKeysArray = Array.from(allKeys);
+  const [filterList, setFilterList] = useState(allKeysArray);
 
   const handleFilterChange = (e) => {
     const { name, value, checked } = e.target;
-    if (checked) appliedFilters.push({ [name]: value });
-    else appliedFilters.splice(appliedFilters.findIndex(filter => filter[name] === value), 1);
-
-    const filteredData = appliedFilters.length > 0
-      ? Data.filter(item => appliedFilters.every(filter => appliedFilters.filter(f => Object.keys(f)[0] === name).some(f => f[name] === item[name]))) : Data;
+    if (checked) {
+      appliedFilters.push({ [name]: value });
+    } else {
+      const index = appliedFilters.findIndex((filter) => filter[name] === value);
+      if (index !== -1) {
+        appliedFilters.splice(index, 1);
+      }
+    }
+    let filteredData;
+    if (appliedFilters.length > 0) {
+      filteredData = Data.filter((item) => {
+        return appliedFilters.every((filter) => {
+          const key = Object.keys(filter)[0];
+          return appliedFilters
+            .filter((f) => Object.keys(f)[0] === key)
+            .some((f) => f[key] === item[key]);
+        });
+      });
+    } else {
+      filteredData = Data;
+    }
     setData(filteredData);
   };
 
   const handleNameFilterChange = (e) => {
-    const value = e.target.value.toLowerCase();
-    const filteredData = value === "" ? Data : data.filter(x => x.name.toLowerCase().includes(value));
-    setData(filteredData);
+    let value = e.target.value.toLowerCase();
+    if (value === "") {
+      setData(Data);
+    } else {
+      let filteredData = data.filter(x => x.name.toLowerCase().includes(value));
+      setData(filteredData);
+    }
   };
 
   return (
     <div>
       <div className="filters-container">
-        {filterList.map(filterName => {
+        {filterList?.map((filterName) => {
           const uniqueValues = new Set();
           let inputRendered = false;
           if (filterName !== "id") {
             return (
               <div className="filter-group">
                 <h3>{filterName.toUpperCase()}</h3>
-                {Data.map(filterValues => {
+                {Data?.map((filterValues) => {
                   if (!uniqueValues.has(filterValues[filterName])) {
                     uniqueValues.add(filterValues[filterName]);
-                    return filterName && filterName.toLowerCase() !== "name"
-                      ? (
-                        <div className="form-check form-switch">
-                          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">{filterValues[filterName]}</label>
-                          <input className="form-check-input" type="checkbox" id="flexSwitchCheckDefault" name={filterName} value={filterValues[filterName]} onChange={handleFilterChange} />
-                        </div>
-                      ) : !inputRendered && (
-                        <>
-                          {inputRendered = true}
-                          <div class="input-group mb-3">
-                            <input type="text" class="form-control" placeholder="Name" onChange={handleNameFilterChange} />
+                    if (filterName && filterName.toLowerCase() !== "name") {
+                      if(filterValues[filterName]){
+                        return (
+                          <div className="form-check form-switch">
+                            <label
+                              className="form-check-label"
+                              htmlFor="flexSwitchCheckDefault"
+                            >
+                              {filterValues[filterName]}
+                            </label>
+                            <input
+                              className="form-check-input"
+                              type="checkbox"
+                              id="flexSwitchCheckDefault"
+                              name={filterName}
+                              value={filterValues[filterName]}
+                              onChange={(e) => handleFilterChange(e)}
+                            />
                           </div>
-                        </>
+                        );
+                      }
+                    } else if (!inputRendered) {
+                      inputRendered = true;
+                      return (
+                        <div class="input-group mb-3">
+                          <input
+                            type="text"
+                            class="form-control"
+                            placeholder="Name"
+                            onChange={(e) => handleNameFilterChange(e)}
+                          />
+                        </div>
                       );
-                  } else return null;
+                    }
+                  } else {
+                    return null;
+                  }
                 })}
               </div>
             );
-          } else return null;
+          } else {
+            return null;
+          }
         })}
       </div>
       <table className="table table-striped">
         <thead>
-          <tr>{headers.map(header => <th key={header}>{header.toUpperCase()}</th>)}</tr>
+          <tr>
+            {headers?.map((headers) => (
+              <th key={headers}>{headers.toUpperCase()}</th>
+            ))}
+          </tr>
         </thead>
         <tbody>
-          {data.map((row, index) => (
+          {data?.map((row, index) => (
             <tr key={index}>
-              {headers.map((header, i) => (
-                row.hasOwnProperty(header)
-                  ? <td key={i}>{row[header]}</td>
-                  : <td key={i} colSpan={headers.length}></td>
-              ))}
+              {headers.map((header, i) => {
+                if (row.hasOwnProperty(header)) {
+                  return <td key={i}>{row[header]}</td>;
+                } else {
+                  const colspan = headers.length;
+                  return (
+                    <td key={i} colSpan={colspan}></td>
+                  );
+                }
+              })}
             </tr>
           ))}
         </tbody>
